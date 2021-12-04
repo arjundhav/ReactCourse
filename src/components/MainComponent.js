03fe4da87beab8bb1f37be7249d69672b3a4fab0
Arjun <arjundhav@gmail.com>
2021-12-04 12:54:03 +0530

    functional components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,43 +1,48 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import Home from "./HomeComponent.js";
 import Menu from "./MenuComponent.js";
-import DishDetail from "./DishdetailComponent.js";
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from "../shared/dishes";
-
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
+import {Switch, Route, Redirect} from 'react-router-dom';
 class Main extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
-
   render() {
+
+    const Homepage=() =>{
+      return(
+        <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]}
+          promotions={this.state.promotions.filter((promo)=> promo.featured)[0]}
+          leaders={this.state.leaders.filter((leader)=> leader.featured)[0]}
+       />
+      );    
+    };
+
     return (
       <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Restro ConFus</NavbarBrand>
-          </div>
-        </Navbar>
 
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-
-        <DishDetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+      <Header />
+       <Switch>
+         <Route path="/home" componet={Homepage} />
+         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
+         <Route exact path='/contactus' component={Contact} />
+         <Redirect to="/home" />
+       </Switch>
+        <Footer />
       </div>
     );
   }
--- src/components/menucomponent.js
+++ src/components/menucomponent.js
@@ -3,51 +3,39 @@ import {
   Card,
   CardImg,
   CardImgOverlay,
-  CardText,
-  CardBody,
   CardTitle,
 } from "reactstrap";
 
 //The components acts as a unit for gathering together a bunch of React elements with a common purpose.
-class Menu extends Component {
-  constructor(props) {
-    super(props);
-  }
+  function RenderMenuItem({dish, onClick}){ 
+    return( 
+      <Card onClick={()=>onClick(dish.id)}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
+    );
+  }          
 
-  renderDish(dish) {
-    if (dish != null)
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else return <div></div>;
-  }
+  
+  const Menu = (props) => {
 
-  render() {
-    const menu = this.props.dishes.map((dish) => {
-      return (
-        <div className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.props.onClick(dish.id)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
-              <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
-        </div>
-      );
+    const menu = props.dishes.map((dish) => {
+        return (
+            <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
+            </div>
+        );
     });
 
     return (
-      <div className="container">
-        <div className="row">{menu}</div>
-      </div>
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
     );
   }
-}
 
 export default Menu;
