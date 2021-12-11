0f599ac31f284ebd9b1972d3472d6375cd11cf6e
Arjun <arjundhav@gmail.com>
2021-12-11 15:38:48 +0530

    Redux Actions

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,36 @@
- import { createStore } from 'redux';
- import { Reducer, initialState } from './reducer';
- 
- // creating store
- export const ConfigureStore = () => {
-     const store = createStore( 
-         Reducer, 
-         initialState
-     );
- 
-     return store;
- }
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 
+import { Dishes  } from './dishes';
+import { Comments  } from './comments';
+import { Promotions  } from './promotions';
+import { Leaders  } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
+
+//// creating store
+export const ConfigureStore = () => {
+
+    const store = createStore(         
+
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders,
+            
+            ...createForms({
+                feedback: InitialFeedback
+            })
+        }),
+        applyMiddleware( thunk, logger )            
+    );
+
+    
+
+    return store;
+}
  /**     REDUX
  *
  *  # Holds the current state
